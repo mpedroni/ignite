9b37a1792605900b7cd0f554ae93d8737dd032d8
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-08 22:52:45 -0300

    Cria um módulo que soma os itens de um array utilizando recursividade

---
+++ elixir/01--sum_list/lib/sum_list.ex
@@ -0,0 +1,13 @@
+defmodule SumList do
+  def call(list), do: sum(list, 0)
+
+  def call_enum(list), do: Enum.sum(list)
+
+  # defp -> private functions
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail], acc) do
+    acc = acc + head
+    sum(tail, acc)
+  end
+end
---
+++ elixir/01--sum_list/test/sum_list_test.exs
@@ -0,0 +1,16 @@
+defmodule SumListTest do
+  use ExUnit.Case
+
+  # test only public functions
+  describe "call/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      expected_response = 6
+
+      assert response == expected_response
+    end
+  end
+end
