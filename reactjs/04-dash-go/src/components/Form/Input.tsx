b72cc487c3e24cc9b2d6c5f629e86070490cc4d9
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-15 10:35:35 -0300

    Configuração do MirageJS + Faker e do React Query

--- reactjs/04-dash-go/src/components/Form/Input.tsx
+++ reactjs/04-dash-go/src/components/Form/Input.tsx
@@ -1,16 +1,17 @@
 import { forwardRef, ForwardRefRenderFunction } from "react";
-import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
-
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps, FormErrorMessage } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string
+  error?: FieldError
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
-  = ({ name, label, ...rest }, ref) => {
+  = ({ name, label, error = null, ...rest }, ref) => {
   return (
-    <FormControl>
-      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+    <FormControl isInvalid={!!error}>
+      { !!label && <FormLabel id={label} htmlFor={name}>{label}</FormLabel> }
       <ChakraInput
         id={name}
         name={name}
@@ -24,6 +25,11 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
         ref={ref}
         {...rest}
       />
+      {!!error && (
+        <FormErrorMessage>
+          {error.message}
+        </FormErrorMessage>
+      )}
     </FormControl>
   );
 }
--- reactjs/04-dash-go/src/pages/users/index.tsx
+++ reactjs/04-dash-go/src/pages/users/index.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
-import { Box, Flex, Heading, Table, Thead, Tbody, Tr, Th, Td, Checkbox, Text, Button, Icon,useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Table, Thead, Tbody, Tr, Th, Td, Checkbox, Text, Button, Icon,useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine } from 'react-icons/ri';
+
+import { useUsers } from '../../services/hooks/useUsers';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, isFetching, error } = useUsers();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -21,7 +25,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" /> }
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
@@ -37,65 +45,51 @@ export default function UserList() {
           </Flex>
 
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8"> 
-                  <Checkbox colorScheme="pink" />
-                </Th>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8"> 
+                      <Checkbox colorScheme="pink" />
+                    </Th>
 
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                <Tbody>
+                  {data.map(user => (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Matheus Pedroni</Text>
-                    <Text fontSize="small" color="gray.300">mpedroni77@gmail.com</Text>
-                  </Box>
-                </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="small" color="gray.300">{user.email}</Text>
+                        </Box>
+                      </Td>
 
-                {isWideVersion && <Td>07 de novembro, 2021</Td>}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Matheus Pedroni</Text>
-                    <Text fontSize="small" color="gray.300">mpedroni77@gmail.com</Text>
-                  </Box>
-                </Td>
-
-                {isWideVersion && <Td>07 de novembro, 2021</Td>}
-              </Tr>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Matheus Pedroni</Text>
-                    <Text fontSize="small" color="gray.300">mpedroni77@gmail.com</Text>
-                  </Box>
-                </Td>
-
-                {isWideVersion && <Td>07 de novembro, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
