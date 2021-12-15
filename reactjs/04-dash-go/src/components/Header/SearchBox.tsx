2cea88a7f72999420e207cf1935831a2635b63ca
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-14 23:04:07 -0300

    Implementação do React Hook Form

--- reactjs/04-dash-go/src/components/Header/SearchBox.tsx
+++ reactjs/04-dash-go/src/components/Header/SearchBox.tsx
@@ -1,7 +1,14 @@
+import { useRef } from 'react';
 import { Flex, Input, Icon } from "@chakra-ui/react"
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
+  // Controlled Components
+  // const [search, setSearch] = useState('');
+
+  // Uncontrolled Components
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <Flex
       as="label"
@@ -23,6 +30,7 @@ export function SearchBox() {
         mr="4"
         placeholder="Buscar na plataforma"
         _placeholder={{ color: 'gray.400' }}
+        ref={searchInputRef}
       />
 
       <Icon as={RiSearchLine} fontSize="20" />
--- reactjs/04-dash-go/src/pages/index.tsx
+++ reactjs/04-dash-go/src/pages/index.tsx
@@ -1,8 +1,20 @@
-import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import { Input } from '../components/Form/Input'
+import { Input } from '../components/Form/Input';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+}
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
   return (
     <Flex
       w="100vw"
@@ -10,21 +22,41 @@ export default function SignIn() {
       align="center"
       justify="center"
     >
-    <Flex
-      as="form"
-      width="100%"
-      maxWidth={360}
-      bg="gray.800"
-      p={8}
-      borderRadius={8}
-      flexDirection="column"
-    >
-      <Stack spacing="4">
-        <Input type="email" name="email" label="E-mail" />
-        <Input type="password" name="password" label="Senha" />
-      </Stack>
-      <Button type="submit" mt={6} colorScheme="pink" size="lg"> Entrar </Button>
-    </Flex>
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Input 
+            type="email" 
+            name="email" 
+            label="E-mail"
+            {...register('email')}
+          />
+          <Input 
+            type="password" 
+            name="password" 
+            label="Senha"
+            {...register('password')}
+          />
+        </Stack>
+
+        <Button 
+          type="submit" 
+          mt={6} 
+          colorScheme="pink" 
+          size="lg"
+          isLoading={formState.isSubmitting}
+        > 
+          Entrar 
+        </Button>
+      </Flex>
     </Flex>
   )
 }
