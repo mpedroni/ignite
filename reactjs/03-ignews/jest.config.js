fea171c9a2f21aa57fff342ae34c9568c4ee6b30
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-21 22:16:05 -0300

    Configurações adicionais do Jest e do Babel

--- reactjs/03-ignews/jest.config.js
+++ reactjs/03-ignews/jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  testIgnorePatterns: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: [
     '<rootDir>/src/tests/setupTests.ts',
   ],
-  transform: {
-    '^.+\\.(js|jsx|tsx)$': '<rootDir>/node_modules/babel-jest'
-  },
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  moduleNameMapper: {
+    "\\.(css|scss|sass)$": "identity-obj-proxy"
+  }
 };
