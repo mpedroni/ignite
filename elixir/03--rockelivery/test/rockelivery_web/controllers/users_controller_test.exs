6d0e6d91bd73e650b1fb22d77f60a54968d49803
Matheus Pedroni <mpedroni77@gmail.com>
2021-12-30 23:37:48 -0300

    Teste do create User

---
+++ elixir/03--rockelivery/test/rockelivery_web/controllers/users_controller_test.exs
@@ -0,0 +1,59 @@
+defmodule RockeliveryWeb.UsersControllerTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Rockelivery.Factory
+
+  describe "create/2" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+      params = %{
+        "age" => 20,
+        "address" => "Rua das Bananeiras, 15",
+        "cep" => "12345678",
+        "cpf" => "12345678900",
+        "email" => "matheus@pedroni.com",
+        "password" => "123456",
+        "name" => "Matheus Pedroni"
+      }
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:created)
+
+      assert %{
+               "message" => "User created",
+               "user" => %{
+                 "address" => "Rua das Bananeiras, 15",
+                 "age" => 20,
+                 "cpf" => "12345678900",
+                 "email" => "matheus@pedroni.com",
+                 "id" => _id
+               }
+             } = response
+    end
+
+    test "when there are some error, returns the error", %{conn: conn} do
+      params = %{
+        "password" => "123456",
+        "name" => "Matheus Pedroni"
+      }
+
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{
+        "message" => %{
+          "address" => ["can't be blank"],
+          "age" => ["can't be blank"],
+          "cep" => ["can't be blank"],
+          "cpf" => ["can't be blank"],
+          "email" => ["can't be blank"]
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+end
