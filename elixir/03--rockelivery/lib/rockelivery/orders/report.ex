3437b6139370a366f3390d386df7ab37065bd92f
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-24 17:57:22 -0300

    Create function to load (lazy) the orders

---
+++ elixir/03--rockelivery/lib/rockelivery/orders/report.ex
@@ -0,0 +1,18 @@
+defmodule Rockelivery.Orders.Report do
+  import Ecto.Query
+
+  alias Rockelivery.{Order, Repo}
+
+  @default_block_size 500
+
+  def create(filename \\ "report.csv") do
+    query = from order in Order, order_by: order.user_id
+
+    Repo.transaction(fn ->
+      query
+      |> Repo.stream(max_rows: @default_block_size)
+      |> Stream.chunk_every(@default_block_size)
+      |> Stream.flat_map(fn chunk -> Repo.preload(chunk, :items) end)
+    end)
+  end
+end
