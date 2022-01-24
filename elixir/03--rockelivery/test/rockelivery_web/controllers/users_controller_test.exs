4951f5ff2ab7823d6ab160ceb9ca311021245c12
Matheus Pedroni <mpedroni77@gmail.com>
2022-01-24 16:34:50 -0300

    Create sign_in function and token generation

--- elixir/03--rockelivery/test/rockelivery_web/controllers/users_controller_test.exs
+++ elixir/03--rockelivery/test/rockelivery_web/controllers/users_controller_test.exs
@@ -5,6 +5,7 @@ defmodule RockeliveryWeb.UsersControllerTest do
   import Rockelivery.Factory
 
   alias Rockelivery.ViaCep.ClientMock
+  alias RockeliveryWeb.Auth.Guardian
 
   describe "create/2" do
     test "when all params are valid, creates the user", %{conn: conn} do
@@ -65,9 +66,17 @@ defmodule RockeliveryWeb.UsersControllerTest do
   end
 
   describe "delete/2" do
+    setup %{conn: conn} do
+      user = insert(:user)
+      {:ok, token, _claims} = Guardian.encode_and_sign(user)
+
+      conn = put_req_header(conn, "authorization", "Bearer #{token}")
+
+      {:ok, conn: conn, user: user}
+    end
+
     test "when there is a user with the given id, deletes the user", %{conn: conn} do
       id = "70171f01-df8e-43e7-af6d-28362eaf94b4"
-      insert(:user)
 
       response =
         conn
